f4c79ddf1d253053417a135a1b60c57386bc601a
AldairCoronel <aldaircr@ciencias.unam.mx>
2020-03-02 18:59:34 -0600

    top-songs view added

--- Musik/music/views.py
+++ Musik/music/views.py
@@ -4,5 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    """Music index.
+
+    Showing welcome message
+    """
     return HttpResponse("Welcome")
-    
+
+def top_songs(request):
+    """Top songs.
+
+    TODO: Show songs by its popularity
+    """
+    return HttpResponse(
+        "<head><title>Canciones populares</title></head>"
+        "<body><h1>Estas son las canciones del momento:</h1></body>"
+    )
