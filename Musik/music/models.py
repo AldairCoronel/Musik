4f37cf682d64eaf67ffd1b4d1dbaea7308b2b07c
AldairCoronel <aldaircr@ciencias.unam.mx>
2020-03-19 23:01:31 -0600

    Artist model created

--- Musik/music/models.py
+++ Musik/music/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    """Artist Model."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Get str representation.""""
+        return self.name
+
+    def __repr__(self):
+        """Get str representation."""
+        return self.__str__()
