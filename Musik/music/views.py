6811ea9523762da25da795402108854493bbc854
AldairCoronel <aldaircr@ciencias.unam.mx>
2020-03-20 23:20:59 -0600

    Im confused

--- Musik/music/views.py
+++ Musik/music/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.template import loader
+from .models import Song
 
 # Create your views here.
 
@@ -41,5 +42,14 @@ class TopSongs(View):
     """
     template = "music/top_songs.html"
     def get(self, request):
-        "GET"
-        return render(request, self.template)
+        """GET method."""
+        songs = Song.objects.all()
+        to_play_id = request.GET.get("to_play", 1)
+        songs_to_play = Song.objects.filter(id = to_play_id)
+        if songs_to_play.count() == 0:
+            to_play = Song.objects.first()
+        else:
+            to_play = songs_to_play.first()
+
+        context = {"songs": songs, "to_play": to_play}
+        return render(request, self.template, context)
