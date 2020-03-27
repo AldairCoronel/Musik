c582ceacf04d139450b54f2254c03073dffba318
AldairCoronel <aldaircr@ciencias.unam.mx>
2020-03-26 18:16:28 -0600

    Managed to create form in artist/create-artist

--- Musik/music/urls.py
+++ Musik/music/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 
 from . import views
 
+
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('top_songs', views.TopSongs.as_view(), name='top_songs'),
+    path('artist/create-artist', views.CreateArtist.as_view(), name='create_art'),
 ]
--- Musik/music/views.py
+++ Musik/music/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import CreateView
 from django.template import loader
-from music.models import Song
+from music.models import Song, Artist
+from django.urls import reverse_lazy
+
+from .forms import ArtistForm
+
 
 # Create your views here.
 
@@ -48,3 +53,13 @@ class TopSongs(View):
         for song in songs:
             print(song.song_file)
         return render(request, self.template, context)
+
+class CreateArtist(CreateView):
+    """Create artist.
+
+    TODO: Enabling users to create and upload an artist
+    """
+    model = Artist
+    form_class = ArtistForm
+    template_name = "music/create_artist.html"
+    success_url = reverse_lazy('index')
